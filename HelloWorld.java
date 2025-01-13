788bc32b6ce1b3ec81724d31f83287b303028eed
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2025-01-13 14:36:58 +0530

    print hello world in java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Welcome to Java");
 		System.out.println("Welcome to Computer Science");
 		System.out.println("Programming is fun");
+		System.out.println("Hello, World!");
 	}
 }
 /*
