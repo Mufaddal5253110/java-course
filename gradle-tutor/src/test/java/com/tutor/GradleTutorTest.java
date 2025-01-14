bd1f5568fd309dc513dbd733a33303c729f39f5c
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2025-01-14 10:50:11 +0530

    Add unit test for verifying no exception was thrown Add maven repository to pull junit library

---
+++ gradle-tutor/src/test/java/com/tutor/GradleTutorTest.java
@@ -0,0 +1,11 @@
+package com.tutor;
+
+import org.junit.Test;
+
+public class GradleTutorTest {
+    
+    @Test
+    public void verifyNoExceptionThrown() {
+        GradleTutor.main(new String[]{});
+    }
+}
