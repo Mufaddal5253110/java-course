32a8b9317754e3349928fbd3c1d668ccf625cd83
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2025-01-22 12:38:32 +0530

    make use of oops and write test cases

--- gradle-tutor/src/test/java/com/tutor/GradleTutorTest.java
+++ gradle-tutor/src/test/java/com/tutor/GradleTutorTest.java
@@ -1,11 +1,30 @@
 package com.tutor;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
+import org.junit.Assert;
 
 public class GradleTutorTest {
 
-    // @Test
-    // public void verifyNoExceptionThrown() {
-    // GradleTutor.main(new String[]{});
-    // }
+    @Test
+    public void testComputeFinalPosition() {
+        Rover rover = new Rover();
+
+        List<List<String>> testCases = Arrays.asList(
+                Arrays.asList("5 5", "1 2 N", "LMLMLMLMM", "1 3 N"),
+                Arrays.asList("5 5", "3 3 E", "MMRMMRMRRM", "5 1 E"),
+                Arrays.asList("7 7", "2 3 S", "MMMLM", "3 0 E"));
+
+        for (List<String> testCase : testCases) {
+            String plateauCoordinates = testCase.get(0);
+            String initialPosition = testCase.get(1);
+            String instructions = testCase.get(2);
+            String expected = testCase.get(3);
+
+            String result = rover.computeFinalPosition(plateauCoordinates, initialPosition, instructions);
+            Assert.assertEquals("Test failed for input: " + testCase, expected, result);
+        }
+    }
 }
